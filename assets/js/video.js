03888630440c6050be83bac40776d382e167434b
Peter Kaufman <PeterJamesKaufman@gmail.com>
2019-06-10 16:09:25 -0400

    Reduced the video.js file size a little bit and added the ability to remember what url to go back to when a user returns to the site

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var URLParams = {
-    lang: "lang", 
-    title: "video", 
-    time: "time",
-    sub: "sub"
-  };
   var DOMElements = {
     languages: document.getElementById("languages"),
     videoList: document.getElementById("videos"),
@@ -13,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function(){
     subtitles: document.getElementById('subtitles'),
     video: undefined
   };
-  var selectedLanguage;
   var videoOptions = {
     id: undefined,
     width: 640,
@@ -21,7 +14,6 @@ document.addEventListener("DOMContentLoaded", function(){
     autoplay: false
   };
   var player;
-  var videos;
   var url = new URL(window.location.href);
 
   // preprocess check 
@@ -32,7 +24,9 @@ document.addEventListener("DOMContentLoaded", function(){
     displaySelectedVideo();
   }
 
-  DOMElements.languages.addEventListener("change", updateLanguage);
+  DOMElements.languages.addEventListener("change", function() {
+    updatePlayer(DOMElements.videoList.value, true);
+  });
   DOMElements.videoList.addEventListener("change", displaySelectedVideo);
   DOMElements.subtitles.addEventListener("change", function() {
     if (DOMElements.subtitles.value == 'off') {
@@ -44,16 +38,9 @@ document.addEventListener("DOMContentLoaded", function(){
   // add appropriate url for the language
   for (var i = 0; i < DOMElements.langBtns.length; i++) {
     DOMElements.langBtns[i].setAttribute("href", fixURLForLanguage(DOMElements.langBtns[i].id));
-  }
-
-  /**
-   * Updates the selected language and updates the player;
-   */
-  function updateLanguage() {
-    var langs = DOMElements.languages.options;
-    selectedLanguage = langs[DOMElements.languages.selectedIndex];
-    videos = DOMElements.videoList.options;
-    updatePlayer(videos[DOMElements.videoList.selectedIndex].value, true);
+    DOMElements.langBtns[i].addEventListener("click", function () {
+      updateStorage(this.href);
+    });
   }
 
   /**
@@ -61,24 +48,19 @@ document.addEventListener("DOMContentLoaded", function(){
    */
   function displaySelectedVideo() {
     // display the selected video
-    videos = DOMElements.videoList.options;
-    var videoName = videos[DOMElements.videoList.selectedIndex].value;
-    var langs = DOMElements.languages.options;
-    selectedLanguage = langs[DOMElements.languages.selectedIndex];
-    // hide the currently selected video
-    if (player === undefined ) {
-      createVimeoPlayer(videoName, selectedLanguage.value);
-      updateURL(URLParams.title, videoName);
-      updateURL(URLParams.lang, selectedLanguage.value);
+    if (player === undefined ) {;
+      createVimeoPlayer(DOMElements.videoList.value, DOMElements.languages.value);
+      updateURL(Window.Vinya.URLParams.title, DOMElements.videoList.value);
+      updateURL(Window.Vinya.URLParams.lang, DOMElements.languages.value);
     } else {
-      updatePlayer(videoName, false);
+      updatePlayer(DOMElements.videoList.value, false);
     }
   }
 
   /**
    * Updates the url to point to the rigth directory for the language selected.
    * @param {String} lang is the language to swap the page to.
-   * @return a string which is the url to use as a link to that language for 
+   * @returns a string which is the url to use as a link to that language for 
    * this page.
    */
   function fixURLForLanguage(lang) {
@@ -97,7 +79,8 @@ document.addEventListener("DOMContentLoaded", function(){
    * @param {Boolean} languageChanged is whether or not the language was changed.
    */
   function updatePlayer(videoName, languageChanged) {
-    if (Window.Vinya[videoName][selectedLanguage.value] === undefined) {
+    console.log('changes:' + DOMElements.languages.value + ', ' + videoName);
+    if (Window.Vinya[videoName][DOMElements.languages.value] === undefined) {
       DOMElements.videoError.innerText = Window.Vinya.errorMsg;
       DOMElements.video = document.querySelector('iframe');
       DOMElements.video.setAttribute('class' , 'hidden');
@@ -105,20 +88,20 @@ document.addEventListener("DOMContentLoaded", function(){
       DOMElements.video = document.querySelector('iframe');
       DOMElements.videoError.innerText = "";  
       DOMElements.video.setAttribute('class' , '');   
-      updateURL(URLParams.lang, selectedLanguage.value);
+      updateURL(Window.Vinya.URLParams.lang, DOMElements.languages.value);
       player.unload().then(function () {
-        videoOptions.id = Window.Vinya[videoName][selectedLanguage.value];
+        videoOptions.id = Window.Vinya[videoName][DOMElements.languages.value];
         player.loadVideo(videoOptions).then( function () {
           player.ready().then(function(){
-            if (languageChanged && url.searchParams.has(URLParams.time)) {
-              player.setCurrentTime(url.searchParams.get(URLParams.time));
+            if (languageChanged && url.searchParams.has(Window.Vinya.URLParams.time)) {
+              player.setCurrentTime(url.searchParams.get(Window.Vinya.URLParams.time));
             }
             getTextTracks(); 
           });
         });
       });
       if (!languageChanged) {
-        updateURL(URLParams.title, videoName);
+        updateURL(Window.Vinya.URLParams.title, videoName);
       }
     }
   }
@@ -130,8 +113,8 @@ document.addEventListener("DOMContentLoaded", function(){
    */
   function updateURL(param, val) {
     // when the video is changed, the time should be reset
-    if (param === URLParams.title) {
-      url.searchParams.delete(URLParams.time);
+    if (param === Window.Vinya.URLParams.title) {
+      url.searchParams.delete(Window.Vinya.URLParams.time);
     }
     if (url.searchParams.has(param)) {
       url.searchParams.set(param, val)
@@ -139,13 +122,14 @@ document.addEventListener("DOMContentLoaded", function(){
       url.searchParams.append(param, val);
     }
     // if the subtitles are turned off or the video is changed, remove the sub param
-    if (url.searchParams.has(URLParams.sub) && (param === URLParams.title || val == 'off' )) {
-      url.searchParams.delete(URLParams.sub);
+    if (url.searchParams.has(Window.Vinya.URLParams.sub) && (param === Window.Vinya.URLParams.title || val == 'off' )) {
+      url.searchParams.delete(Window.Vinya.URLParams.sub);
     }
     // update the browser history
     if (history.pushState) {
       window.history.pushState({path:url.href},'',url.href);
     }
+    updateStorage(url.href.substring(0, url.href.indexOf('?')));
   }
 
   /**
@@ -154,36 +138,40 @@ document.addEventListener("DOMContentLoaded", function(){
    */
   function parseURL() {
    var params = url.searchParams;
-   var title = params.get(URLParams.title), lang = params.get(URLParams.lang), time = params.get(URLParams.time)
-      sub = params.get(URLParams.sub);
+   var title = params.get(Window.Vinya.URLParams.title), lang = params.get(Window.Vinya.URLParams.lang), time = params.get(Window.Vinya.URLParams.time)
+      sub = params.get(Window.Vinya.URLParams.sub);
    // check to see if the language and video title are present in the url
-   if (params.has(URLParams.title) && params.has(URLParams.lang)) {
+   if (params.has(Window.Vinya.URLParams.title) && params.has(Window.Vinya.URLParams.lang)) {
       // create the iframe and set the default value of the selects
       DOMElements.videoList.value = title;
       DOMElements.languages.value = lang;
-    } else if (params.has(URLParams.title)) {
+    } else if (params.has(Window.Vinya.URLParams.title)) {
       // if the title is present but not the language, use the default language
       DOMElements.videoList.value = title;
       lang = DOMElements.languages.value;
-    } else if (params.has(URLParams.lang)) {
+    } else if (params.has(Window.Vinya.URLParams.lang)) {
       // if the language is present but not the title
       DOMElements.languages.value = lang;
       title = DOMElements.videoList.value;
     }
     createVimeoPlayer(title, lang);
     // check to see if the time is in the url, if so the video will be set to that time
-    if (params.has(URLParams.time)) {
+    if (params.has(Window.Vinya.URLParams.time)) {
       player.setCurrentTime(time);
     }
-    if (params.has(URLParams.sub)) {
+    if (params.has(Window.Vinya.URLParams.sub)) {
       player.enableTextTrack(sub);
     }
   }
 
+  /**
+   * Checks to see if the URL contains any parameters.
+   * @returns whether or not the url has any url parameters in it.
+   */
   function URLContainsParam() {
     var paramExists = false;
-    Object.keys(URLParams).forEach(function (key) {
-      if (url.searchParams.has(URLParams[key])) {
+    Object.keys(Window.Vinya.URLParams).forEach(function (key) {
+      if (url.searchParams.has(Window.Vinya.URLParams[key])) {
         paramExists = true;
       }
     });
@@ -202,13 +190,12 @@ document.addEventListener("DOMContentLoaded", function(){
     DOMElements.video = document.querySelector('iframe');
     player.on("timeupdate", updateURLTime); // updates the url time when progress is made in the video
     player.on("texttrackchange", function (lang) {
-      console.log("text rack changed:" + lang);
       if (!lang.language) {
         // remove the sub from the url
-        updateURL(URLParams.sub, 'off');
+        updateURL(Window.Vinya.URLParams.sub, 'off');
         DOMElements.subtitles.value = 'off';
       } else {
-        updateURL(URLParams.sub, lang.language);
+        updateURL(Window.Vinya.URLParams.sub, lang.language);
         DOMElements.subtitles.value = lang.language;
       }
     });
@@ -217,7 +204,6 @@ document.addEventListener("DOMContentLoaded", function(){
         var trackSelected = false;
         var lang;
         for (var i = 0; i < tracks.length; i++) {
-          console.log(tracks[i]);
           if (tracks[i].mode === 'showing') {
             lang = tracks[i].language;
             trackSelected = true;
@@ -225,10 +211,10 @@ document.addEventListener("DOMContentLoaded", function(){
           }
         }
         if (trackSelected) {
-          updateURL(URLParams.sub, lang);
+          updateURL(Window.Vinya.URLParams.sub, lang);
           DOMElements.subtitles.value = lang;
         } else {
-          updateURL(URLParams.sub, 'off');
+          updateURL(Window.Vinya.URLParams.sub, 'off');
           DOMElements.subtitles.value = 'off';
         }
     })
@@ -242,10 +228,15 @@ document.addEventListener("DOMContentLoaded", function(){
    */
   function updateURLTime() {
     player.getCurrentTime().then(function(seconds) {
-      updateURL(URLParams.time, seconds);
+      updateURL(Window.Vinya.URLParams.time, seconds);
     })
   }
   
+  /**
+   * Gets the subtitles and captions for the video and then adds them to the
+   * select for subtitles. It also selects the subtitle value listed in url 
+   * parameter if it exists. 
+   */
   function getTextTracks() {
     // remove all added tracks
     var elements = document.querySelectorAll('.added');
@@ -254,7 +245,6 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     // add the new ones
     player.getTextTracks().then(function(tracks) {
-      console.log(tracks);
       var subtitle;
       if (tracks.length !== 0) {
         // append all options to the list
@@ -264,10 +254,19 @@ document.addEventListener("DOMContentLoaded", function(){
             DOMElements.subtitles.innerHTML += subtitle;
           }
         }
-        if (url.searchParams.has(URLParams.sub)) {
-          DOMElements.subtitles.value = url.searchParams.get(URLParams.sub);
+        if (url.searchParams.has(Window.Vinya.URLParams.sub)) {
+          DOMElements.subtitles.value = url.searchParams.get(Window.Vinya.URLParams.sub);
         }
       }
     });
+  }
+
+  /**
+   * Updates the url storage value.
+   * @param {String} href is the url to redirect to if a user comes back to the
+   * site.
+   */
+  function updateStorage(href) {
+    localStorage.setItem('url', href);
   }
 });
--- user/pages/01.home/default.md
+++
@@ -1,4 +0,0 @@
----
-title: Home
-body_classes: title-center title-h1h2
----
